cddc06e780cc87dcd5ffc477f3c54d1c84c2a165
SaandeepBalla <163757390+klu2300032775@users.noreply.github.com>
2025-09-16 12:04:26 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/cart";
+const API_URL = "http://98.88.6.234/api/cart";
 
-const API_BASE = "http://localhost:8081/api/cart";
+const API_BASE = "http://98.88.6.234/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
